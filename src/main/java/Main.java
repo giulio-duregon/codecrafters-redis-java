f5fda81126ab2b01a85b4bd1e6b385654afe1ff3
Gduregon <gjd9961@nyu.edu>
2023-11-28 10:44:43 -0500

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,7 +6,7 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
+    final String CRLF = "\r\n";
     //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
@@ -16,6 +16,14 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null){
+                out.write("+PONG"+CRLF);
+                out.flush();
+
+            }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {

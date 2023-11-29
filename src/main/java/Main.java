3985d5f6d12685309365344573b046938a693373
Gduregon <gjd9961@nyu.edu>
2023-11-28 23:09:14 -0500

    adding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,41 +3,43 @@ import resp.RespData;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.Instant;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class Main {
-  public static void main(String[] args){
-      Logger logger = Logger.getLogger(Main.class.toString());
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.toString());
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
-    //  Uncomment this block to pass the first stage
+//  Uncomment this block to pass the first stage
         ServerSocket serverSocket;
         int port = 6379;
         try {
             ConcurrentHashMap<RespData, RespData> db = new ConcurrentHashMap<>();
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          while (true){
-              // Wait for connection from client.
-              Socket clientSocket = serverSocket.accept();
-              executorService.execute(() -> {
-                  try {
-                      new RedisClient(clientSocket, db).run();
-                  } catch (IOException e) {
-                      throw new RuntimeException(e);
-                  }
-              });
-          }
+            ConcurrentHashMap<RespData, Instant> keyExpiry = new ConcurrentHashMap<>();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            while (true) {
+                // Wait for connection from client.
+                Socket clientSocket = serverSocket.accept();
+                executorService.execute(() -> {
+                    try {
+                        new RedisClient(clientSocket, db, keyExpiry).run();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
 
         } catch (IOException e) {
             logger.throwing(Main.class.toString(), "main", new IOException());
-          System.out.println("IOException: " + e.getMessage());
+            System.out.println("IOException: " + e.getMessage());
         }
-  }
+    }
 }
--- src/main/java/RedisClient.java
+++ src/main/java/RedisClient.java
@@ -2,6 +2,7 @@ import resp.*;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
@@ -10,34 +11,39 @@ import java.util.logging.Logger;
 import static resp.RedisCommands.*;
 import static resp.RespConstants.*;
 
-public class RedisClient implements Runnable{
+public class RedisClient implements Runnable {
     private final Logger logger = Logger.getLogger(RedisClient.class + "-Thread-" + Thread.currentThread().getName());
     private final Socket socket;
     private final BufferedReader in;
     private final BufferedWriter out;
 
     private final ConcurrentHashMap<RespData, RespData> db;
+    private final ConcurrentHashMap<RespData, Instant> keyExpiry;
 
-    public RedisClient(Socket socket,ConcurrentHashMap<RespData, RespData> db) throws IOException {
+    public RedisClient(Socket socket,
+                       ConcurrentHashMap<RespData, RespData> db,
+                       ConcurrentHashMap<RespData, Instant> keyExpiry) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         this.out = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
         this.db = db;
+        this.keyExpiry = keyExpiry;
     }
 
-    private RedisCommands parseCommand(String data){
+    private RedisCommands parseCommand(String data) {
         String commandString = data.toLowerCase();
         RedisCommands command;
-        if (commandString.equals("ping")){
+        if (commandString.equals("ping")) {
             command = PING;
-        } else if (commandString.equals("echo")){
+        } else if (commandString.equals("echo")) {
             command = ECHO;
-        } else if (commandString.equals("get")){
+        } else if (commandString.equals("get")) {
             command = GET;
-        } else if (commandString.equals("set")){
-        command = SET;
+        } else if (commandString.equals("set")) {
+            command = SET;
+        } else {
+            command = INVALID;
         }
-        else {command = INVALID;}
         logger.log(Level.INFO, "Parsed String: %s Command: %s".formatted(commandString, command));
         return command;
     }
@@ -46,55 +52,103 @@ public class RedisClient implements Runnable{
         out.write(data.toRespString());
         out.flush();
     }
+
     private void handlePing() throws IOException {
         write(PONG);
     }
 
-    private void handleEcho(RespData response) throws IOException{
+    private void handleEcho(RespData response) throws IOException {
         write(response);
     }
 
+    private boolean keyExpired(RespData key) {
+        Instant value = this.keyExpiry.get(key);
+        if (value != null && value.isBefore(Instant.now())) {
+            this.db.remove(key);
+            this.keyExpiry.remove(key);
+            return true;
+        }
+        return false;
+    }
+
     private void handleGet(RespData key) throws IOException {
-        logger.log(Level.FINE, "Handling GET operation for key %s".formatted(key.toString()));
+        logger.log(Level.INFO, "Handling GET operation for key %s".formatted(key.toString()));
         RespData value = db.get(key);
-        if (value == null){
+        if (keyExpired(key) || value == null) {
             value = NULLBULKRESP;
         }
         write(value);
     }
-    private void handleSet(RespData key, RespData value){
-        logger.log(Level.FINE, "Handling SET operation for key %s, Value %s".formatted(key.toString(), value.toString()));
-        try {
-            db.put(key, value);
-            write(OK);
-        } catch (IOException e) {
-            throw new RuntimeException(e + " Failed during handleSet");
-        }
+
+    private boolean moreArgsPresent(RespArray commandArray) {
+        return commandArray.dataArray().size() >= 2;
     }
+
+    private Instant getKeyExpireTime(RespArray commandArr, String unit) {
+        // Parse the arguments to determine unit of time for key expiry
+
+        // MilliSeconds
+        if (unit.equals("px")) {
+            return Instant.ofEpochMilli(Integer.parseUnsignedInt(commandArr.popFront().inputString()));
+            // Seconds
+        } else if (unit.equals("ex")) {
+            return Instant.ofEpochSecond(Integer.parseUnsignedInt(commandArr.popFront().inputString()));
+        }
+        // Unsure of difference between PX / EXAT args
+        return Instant.ofEpochMilli(Integer.parseUnsignedInt(commandArr.popFront().inputString()));
+    }
+
+    private void Set(RespData key, RespData value) throws IOException {
+        this.db.put(key, value);
+        write(OK);
+    }
+
+    private void Set(RespData key, RespData value, Instant expTime) throws IOException {
+        this.db.put(key, value);
+        this.keyExpiry.put(key, expTime);
+        write(OK);
+    }
+
+    private void handleSet(RespArray commandArray) throws IOException {
+        // Grab the KV pair from args passed
+        RespData key = commandArray.popFront();
+        RespData value = commandArray.popFront();
+        logger.log(Level.INFO, "Handling SET operation for key %s, Value %s".formatted(key.toString(), value.toString()));
+
+        // Check if there are additional optional arguments for time expiry
+        if (moreArgsPresent(commandArray)) {
+            String unit = commandArray.popFront().inputString();
+            Instant expTime = getKeyExpireTime(commandArray, unit.toLowerCase());
+            Set(key, value, expTime);
+            return;
+        }
+        Set(key, value);
+    }
+
     private void handleCommand(RedisCommands command, RespArray commandArray) throws IOException {
-    switch (command){
-        case PING -> handlePing();
-        case ECHO -> handleEcho(commandArray.popFront());
-        case SET -> handleSet(commandArray.popFront(), commandArray.popFront());
-        case GET -> handleGet(commandArray.popFront());
+        switch (command) {
+            case PING -> handlePing();
+            case ECHO -> handleEcho(commandArray.popFront());
+            case SET -> handleSet(commandArray);
+            case GET -> handleGet(commandArray.popFront());
         }
     }
 
     private RespArray parseRespArray(int size) throws IOException {
         ArrayList<RespData> arr = new ArrayList<>();
         String inputLine;
-        for(int i =0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             inputLine = in.readLine();
             arr.add(parse(inputLine));
         }
         return new RespArray(arr);
     }
 
-    private RespData parse(String s) throws IOException{
+    private RespData parse(String s) throws IOException {
         RespData request;
-        switch (s.charAt(0)){
+        switch (s.charAt(0)) {
             // Resp Simple String
-            case '+' -> request = new RespSimpleString(s.substring(1,s.length()-2));
+            case '+' -> request = new RespSimpleString(s.substring(1, s.length() - 2));
             // Resp Bulk String
             case '$' -> request = new RespBulkString(in.readLine());
             case '*' -> request = parseRespArray(Character.getNumericValue(s.charAt(1)));
@@ -107,30 +161,29 @@ public class RedisClient implements Runnable{
         try {
             String inputLine;
             RedisCommands command;
-            int i = 0;
-            while ((inputLine = in.readLine()) != null){
-                if (inputLine.isEmpty()){
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.isEmpty()) {
                     continue;
                 }
-                    RespArray commandArray = (RespArray) parse(inputLine);
-                    RespBulkString rawCommand = commandArray.popFront();
-                    command = parseCommand(rawCommand.inputString());
-                    handleCommand(command, commandArray);
-                }
-            } catch (IOException e){
-                logger.log(Level.SEVERE, "Failed during run in Thread "
-                        + Thread.currentThread().getName()  +
-                        ", exception=" + e.getMessage());
+                RespArray commandArray = (RespArray) parse(inputLine);
+                RespBulkString rawCommand = commandArray.popFront();
+                command = parseCommand(rawCommand.inputString());
+                handleCommand(command, commandArray);
             }
-            logger.log(Level.INFO, "Finished execution");
-            close();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed during run in Thread "
+                    + Thread.currentThread().getName() +
+                    ", exception=" + e.getMessage());
+        }
+        logger.log(Level.INFO, "Finished execution");
+        close();
     }
 
-    private void close(){
+    private void close() {
         try {
-        in.close();
-        out.close();}
-        catch (IOException e){
+            in.close();
+            out.close();
+        } catch (IOException e) {
             logger.log(Level.SEVERE,
                     "Failing to close input and output streams in thread: " + Thread.currentThread().getName());
         }

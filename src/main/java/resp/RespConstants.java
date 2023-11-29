5ac7b878b87cdc41adcf2c28c1b95afbc8e25a14
Gduregon <gjd9961@nyu.edu>
2023-11-29 09:45:44 -0500

    null

--- src/main/java/resp/RespConstants.java
+++ src/main/java/resp/RespConstants.java
@@ -3,6 +3,6 @@ package resp;
 public class RespConstants {
     public static final String CRLF = "\r\n";
     public static final RespSimpleString PONG = new RespSimpleString("PONG");
-    public static final RespBulkString NULLBULKRESP = new RespBulkString("$-1");
+    public static final RespBulkString NULLBULKRESP = new RespBulkString("$-1" + CRLF);
     public static final RespSimpleString OK = new RespSimpleString("OK");
 }
